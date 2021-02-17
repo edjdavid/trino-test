60769af1e14cfab850f3c3b99c971f413795b2dd
Eduardo David Jr <edjdavid@users.noreply.github.com>
2021-02-17 22:28:52 +0800

    Add create table example

--- test_trino.sql
+++ test_trino.sql
@@ -1,6 +1,18 @@
 CREATE SCHEMA IF NOT EXISTS hive.default;
 USE hive.default;
 
+CREATE TABLE orders (
+  order_key bigint,
+  order_status varchar,
+  total_price double,
+  order_date date
+)
+WITH (
+	format = 'ORC'
+);
+
+INSERT INTO orders VALUES (1, 'A', 1.0, DATE '2021-01-30');
+
 SELECT *
 FROM orders;
 
